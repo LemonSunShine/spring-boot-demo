b6487a2a49c8e93cf4f8967f93addabe2783d7d8
zhangwei <zhangwei@lvmama.com>
2018-01-31 18:07:48 +0800

    初始化工程

---
+++ src/main/java/com/sbw/springboot/demo/config/JdbcConfig.java
@@ -0,0 +1,45 @@
+package com.sbw.springboot.demo.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+/**
+ * 数据源配置,配置多数据源
+ * Created by sbw on 2017/11/30.
+ */
+@Configuration
+@EnableTransactionManagement
+public class JdbcConfig {
+
+    @Bean(name = "oneDataSource")
+    @Qualifier("oneDataSource")
+    @Primary
+    @ConfigurationProperties(prefix="spring.datasource.one")
+    public DataSource oneDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "twoDataSource")
+    @Qualifier("twoDataSource")
+    @ConfigurationProperties(prefix="spring.datasource.two")
+    public DataSource twoDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "oneJdbcTemplate")
+    public JdbcTemplate oneJdbcTemplate(@Qualifier("oneDataSource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+    @Bean(name = "twoJdbcTemplate")
+    public JdbcTemplate twoJdbcTemplate(@Qualifier("twoDataSource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
